f918963c35e8f4f6ef23196341b10d95101ff8ba
natnaelajema <nhattybirhanu@gmail.com>
2025-04-12 05:30:48 -0500

    fix routes and scripts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -7,7 +7,7 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: 'challenge-pack/:id', renderMode: RenderMode.Prerender,
     async getPrerenderParams() {
-      const res = await fetch('https://challenger-stage-a6c8f9714982.herokuapp.com/api/v1/public/challenges/title');
+      const res = await fetch('https://challenger-stage-a6c8f9714982.herokuapp.com/api/v1/public/challenges/titles');
       const ids: string[] = await res.json();
       return ids.map(id => ({ id }));// Generates paths like: [{ id: '1' }, { id: '2' }, { id: '3' }]
     }
