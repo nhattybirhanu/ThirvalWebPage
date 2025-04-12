83e85350835965ec1af1097fa736887a1e873e89
natnaelajema <nhattybirhanu@gmail.com>
2025-04-12 06:14:43 -0500

    fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,7 +6,7 @@ export const serverRoutes: ServerRoute[] = [
     path: 'challenge-pack/:id', renderMode: RenderMode.Prerender,
     async getPrerenderParams() {
       const res = await fetch('https://challenger-stage-a6c8f9714982.herokuapp.com/api/v1/public/challenges/titles');
-      console.log("routes res ",res)
+      console.log("routes res ",res.json())
       const ids: string[] = await res.json();
       return ids.map(id => ({ id }));// Generates paths like: [{ id: '1' }, { id: '2' }, { id: '3' }]
     }
