3044fd08e44e9b37a56524c37ad2014e47f3a6d2
natnaelajema <nhattybirhanu@gmail.com>
2025-04-16 20:18:12 -0500

    update vercel api index.mjs

--- api/index.mjs
+++ api/index.mjs
@@ -1,7 +1,7 @@
-// api/index.mjs
 import bootstrap from '../dist/thrival-web-page/server/server.mjs';
 
-export default async function handler(req, res) {
-  const { handler } = await bootstrap(); // Bootstrap SSR
-  return handler(req, res); // Delegate request to Angular SSR
+const server = await bootstrap();
+
+export default function handler(request, response) {
+  return server.handler(request, response);
 }
