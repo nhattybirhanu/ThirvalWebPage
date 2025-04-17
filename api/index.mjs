8c912abbb089333794622639e49bc9b3e120e817
natnaelajema <nhattybirhanu@gmail.com>
2025-04-17 16:43:13 -0500

    update function name

--- api/index.mjs
+++ api/index.mjs
@@ -1,11 +1,11 @@
 import handler from '../dist/thrival-web-page/server/server.mjs';
-import('../dist/thrival-web-page/server/main.js')
+import('../dist/thrival-web-page/server/server.mjs')
 .then(module => module.app)
   .catch(error => {
     console.error('Failed to load server module:', error);
     throw error;
   });
 export default async (req, res) => {
-  const { app } = await import('../dist/thrival-web-page/server/main.js');
+  const { app } = await import('../dist/thrival-web-page/server/server.mjs');
   return app(req, res);
 };
