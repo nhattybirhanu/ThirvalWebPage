afed0a6fe9c8167ba0f4581fc61f2275536dc1c4
natnaelajema <nhattybirhanu@gmail.com>
2025-04-17 01:51:46 -0500

    update vercel api index.mjs

--- api/index.mjs
+++ api/index.mjs
@@ -1,7 +1,5 @@
-import bootstrap from '../dist/thrival-web-page/server/server.mjs';
 
-const server = await bootstrap();
 
-export default function handler(request, response) {
-  return server.handler(request, response);
-}
+
+export default import('../dist/thrival-web-page/server/server.mjs')
+  .then(module => module.app());
