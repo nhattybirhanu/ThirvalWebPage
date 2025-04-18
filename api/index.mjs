110c49eefc7e3306fc91fc7e0c4027c03967a636
natnaelajema <nhattybirhanu@gmail.com>
2025-04-17 21:14:25 -0500

    fix

--- api/index.mjs
+++ api/index.mjs
@@ -1,15 +1,13 @@
-// import handler from '../dist/thrival-web-page/server/server.mjs';
-// import('../dist/thrival-web-page/server/server.mjs')
-// .then(module => module.app)
-//   .catch(error => {
-//     console.error('Failed to load server module:', error);
-//     throw error;
-//   });
-// export default async (req, res) => {
-//   const { app } = await import('../dist/thrival-web-page/server/server.mjs');
-//   return app(req, res);
-// };
+import handler from '../dist/thrival-web-page/server/server.mjs';
+import('../dist/thrival-web-page/server/server.mjs')
+.then(module => module.app)
+  .catch(error => {
+    console.error('Failed to load server module:', error);
+    throw error;
+  });
+export default async (req, res) => {
+  const { app } = await import('../dist/thrival-web-page/server/server.mjs');
+  return app(req, res);
+};
 // api/index.mjs
-import handler from '../dist/thrival-web-page/server/main.js';
 
-export default handler;
