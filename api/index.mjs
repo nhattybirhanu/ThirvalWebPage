b4ec812a2f4f77f3c941b6cd536dfabb500ffbb5
natnaelajema <nhattybirhanu@gmail.com>
2025-05-23 13:50:47 -0500

    add redirect  test

--- api/index.mjs
+++ api/index.mjs
@@ -1,9 +1,6 @@
 import handler from '../dist/thrival-web-page/server/server.mjs';
 
 export default async (req, res) => {
-  if (req.url === '/.well-known/apple-app-site-association') {
-    return;
-  }
   return handler(req, res);
 };
 
